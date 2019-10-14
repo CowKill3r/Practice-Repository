2be3597b61ed47dc55d4fbf383c5e53df504df03
Octavian Stoch <stoch.octavian@gmail.com>
2019-10-14 15:39:34 -0500

    completed finding the number of palindromes and their sums

--- Daily Problems/Daily Problems In Java/src/number_of_palindromes.java
+++ Daily Problems/Daily Problems In Java/src/number_of_palindromes.java
@@ -9,34 +9,52 @@ import java.util.*;
 
 public class number_of_palindromes {
 	
-	public static int palindromeStarter;
+	public static int palindromeStarter, reversedInt, firstPass, numPasses;
 	
-	public static void reverseInt(String n) {
+	public static boolean checkForPalindrome(int n) {
+		/*
+		 * check the given integer for a palindrome
+		 * e.g. 456 = false , 9339 = true
+		 * */
+		if (n == reverseInt(Integer.toString(n))) {
+			System.out.println("The given integer is a Palindrome!");
+			return true; 	//if the given integer that's reversed is the same
+			 				//when run through reverseInt, return true
+		}
+		else {
+			System.out.println("The given integer isn't a Palindrome!");
+			return false;  //if not the same, return false and keep on going
+		}
+	}
+	
+	public static int reverseInt(String n) {
 		/*
 		 * Approach: 
 		 * 1) Make a new stack of integers
 		 * 2) loop through string of integer (toString()) using charAtpos()
 		 * 3) return new reversed integer
 		 * */
+		System.out.println("n is: " + n);
 		Stack<String>		reverseMyInteger 	= new Stack<String>();
 		ArrayList<String> 	integerList	= new ArrayList<String>();
 		
 		for (int i = 0; i < n.length(); i++) {
 			String addThis = String.valueOf(n.charAt(i));
 			reverseMyInteger.push(addThis);
-			System.out.println(n.charAt(i));
+			//System.out.println(n.charAt(i));
 		}
 		
 		System.out.println("The stack contains: " + reverseMyInteger);
 		
-		for (int i = 0; i <= reverseMyInteger.size()+1; i++) {
+		while (reverseMyInteger.size() != 0) {
 			//String newIntegerValue = String.valueOf(n.charAt(i));
 			//makeNewInteger.add(newIntegerValue);
 			integerList.add(reverseMyInteger.pop());
+			System.out.println("Integer List is: " + integerList);
 		}
+		System.out.println("\n");
 		
-	
-		System.out.println("The new stack contains: " + reverseMyInteger);
+		//System.out.println("The new stack contains: " + reverseMyInteger);
 		
 		StringBuilder sb = new StringBuilder();
 		
@@ -47,8 +65,27 @@ public class number_of_palindromes {
 		
 		int finalReversed = Integer.parseInt(sb.toString());
 		
-		System.out.println("Given integer is: " + n +"\nReversed Intger is: " + finalReversed + "\n Sum is: "
-				+ (Math.addExact(palindromeStarter, finalReversed)));
+		if (firstPass != 1) {
+			System.out.println("DOING FIRST PASS");
+			System.out.println("Given integer is: " + n +"\nReversed Intger is: " + finalReversed + "\n Sum is: "
+					+ (Math.addExact(palindromeStarter, finalReversed)));
+			numPasses += 1;
+			reversedInt = Math.addExact(palindromeStarter, finalReversed);
+			
+			firstPass = 1;
+			return finalReversed;
+		}
+		
+		else {
+			System.out.println("DOING ALL OTHER PASSES");
+			System.out.println("Given integer is: " + n +"\nReversed Intger is: " + finalReversed + "\n Sum is: "
+					+ (Math.addExact(reversedInt, finalReversed)));
+			numPasses += 1;
+			reversedInt = Math.addExact(reversedInt, finalReversed);
+			return finalReversed;
+		}
+		
+		
 		
 	}
 
@@ -59,6 +96,7 @@ public class number_of_palindromes {
 		
 		palindromeStarter 			= sc.nextInt();
 		String 	convertedStarter 	= Integer.toString(palindromeStarter);
+		sc.close();
 		
 		reverseInt(convertedStarter);
 		
@@ -68,6 +106,10 @@ public class number_of_palindromes {
 	
 	public static void main(String[] args) {
 		solution();
+		while (checkForPalindrome(reversedInt) != true) {
+			reverseInt(Integer.toString(reversedInt));
+		}
+		System.out.println("Total number of passes: " + numPasses);
 	}
 
 }
