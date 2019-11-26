5f2a43420d3dcbe67cfaf53c4da6751c1339517a
Octavian Stoch <stoch.octavian@gmail.com>
2019-11-26 17:46:01 -0600

    made sure to prevent arrayIndexOutOfBounds exceptions and negatives and added comments to algorithm

--- Daily Problems/Daily Problems In Java/src/sublist_sum_of_list.java
+++ Daily Problems/Daily Problems In Java/src/sublist_sum_of_list.java
@@ -10,14 +10,28 @@ public class sublist_sum_of_list {
 	private static int sum(List<Integer> L, int i,int j) {
 		System.out.println(L+"\n"+"index i = "+i+"\n"+"index j = "+j);
 		List<Integer> subListToSum = new ArrayList<Integer>();
-		subListToSum.addAll(L.subList(i, j)); //sublist include i, exclude j
-		int result = 0;
 		
-		for (int allInts : subListToSum) {
-			result += allInts;
-		};
+		if (i <= j) { //make sure that i is less than j so we have a sublist of at least one
+			subListToSum.addAll(L.subList(i, j));
+			
+		}
 		
-		System.out.println("Sum(" + i + ", " + j + ")" + " = " + result);
+		else if (i > j) { //print out a error so the user knows what they did wrong
+			System.out.println("index 'i' is more than index 'j'");
+		}
+		
+		int sizeList = L.size(); //only reaches here if i <= j
+		System.out.println("size of list is: " + sizeList);
+		if (j <= sizeList & i <= sizeList) { //check to account for out of bounds and negatives
+			int result = 0;
+			
+			for (int allInts : subListToSum) {
+				result += allInts;
+			};
+			
+			System.out.println("Sum(" + i + ", " + j + ")" + " = " + result);
+		}
+		
 		return 0;
 	}
 
