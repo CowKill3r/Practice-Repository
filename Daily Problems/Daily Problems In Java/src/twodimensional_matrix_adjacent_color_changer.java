c8ac7b4471b71cafb99c123f2b6968a7cde7ca74
Octavian Stoch <stoch.octavian@gmail.com>
2019-11-28 15:23:38 -0600

    changes all adjacent cells if the color is the same as the one that was changed, still need to have it look at each direction + all the directions around those

--- Daily Problems/Daily Problems In Java/src/twodimensional_matrix_adjacent_color_changer.java
+++ Daily Problems/Daily Problems In Java/src/twodimensional_matrix_adjacent_color_changer.java
@@ -22,6 +22,15 @@ public class twodimensional_matrix_adjacent_color_changer {
 		B B B
 	 */
 	
+	private static void printColors (String[][] s) {
+		for (int i = 0; i < s.length; i++) {
+			for (int j = 0; j < s[i].length; j++) {
+				System.out.print(s[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}
+	
 	private static int replaceColor(String color, int x, int y) { 
 		ArrayList<String> colorList = new ArrayList<String>();
 		colorList.add("R");
@@ -40,6 +49,38 @@ public class twodimensional_matrix_adjacent_color_changer {
 		}
 		
 		System.out.println(colorList + "\n");
+		
+		/*Approach: here you do a recursive? 
+		 * 1) nested for loop through the 2d array of strings
+		 * 2) check is the thing != color 
+		 * 		2a) then: thing = color
+		 * 3) else: function (array)*/
+		
+		int colorchange = 0;
+		String lookAtMe = pixels[x][y];
+		for (int i = 0; i < pixels.length; i++) { //rows
+			for (int j = 0; j < pixels[i].length; j++) { //columns
+				if (!pixels[x][y].matches(color)) {
+					pixels[x][y] = color;
+				}
+				else {
+					if (pixels[x][y+1].matches(lookAtMe)) { //direction = south
+						pixels[x][y+1] = color;
+					}
+					if (pixels[x][y-1].matches(lookAtMe)) { //direction = north
+						pixels[x][y-1] = color;
+					}
+					if (pixels[x+1][y].matches(lookAtMe)) { //direction = east
+						pixels[x+1][y] = color;
+					}
+					if (pixels[x-1][y].matches(lookAtMe)) { //direction = west
+						pixels[x-1][y] = color;
+					}
+				}
+			}
+		}
+		
+		printColors(pixels);
 		
 		return 0;
 	}
