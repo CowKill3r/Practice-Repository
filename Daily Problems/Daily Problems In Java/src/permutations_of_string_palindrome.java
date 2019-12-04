0728d23022de86983a92e3902b889456f17c3966
Octavian Stoch <stoch.octavian@gmail.com>
2019-12-03 18:58:14 -0600

    more work on permutations of string palindromes to make it faster & all edge cases

--- Daily Problems/Daily Problems In Java/src/permutations_of_string_palindrome.java
+++ Daily Problems/Daily Problems In Java/src/permutations_of_string_palindrome.java
@@ -1,9 +1,45 @@
+import java.util.ArrayList;
 
 public class permutations_of_string_palindrome {
 	
+	private static ArrayList<String> listOfPals = new ArrayList<String>();
+
 	private static boolean solution(String in) {
-		System.out.println(in);
-		return isPalindrome(in);
+		System.out.println("Input is " + in);
+		allPermutations(in, 0, in.length()-1);
+		System.out.println(listOfPals + "\n" + listOfPals.size());
+		for (String s : listOfPals) {
+			System.out.println(s);
+		}
+		
+		return true;
+	}
+	
+	private static void allPermutations(String in, int l, int r) {
+		if (l == r) {
+			System.out.println(in);
+			
+		}
+		else {
+			for (int i = 0; i <= r; i++) {
+				in = swap(in, l, i);
+				allPermutations(in, l+1, r);
+				in = swap(in, l, i);
+			}
+			if (isPalindrome(in)) {
+				listOfPals.add(in);
+			}
+		}
+		//System.out.println(listOfPals);
+	}
+	
+	private static String swap(String in , int i, int j) {
+		char t;
+		char[] charArr = in.toCharArray();
+		t = charArr[i];
+		charArr[i] = charArr[j];
+		charArr[j] = t;
+		return String.valueOf(charArr);
 	}
 	
 	private static boolean isPalindrome(String in) {
@@ -18,12 +54,12 @@ public class permutations_of_string_palindrome {
 		}
 		
 		if (in.equals(out)) {
-			System.out.println("Input: " + in + " is the same as Output: " + out);
+			//System.out.println("Input: " + in + " is the same as Output: " + out);
 			result = true;
 			return result;
 		}
 		else {
-			System.out.println("Input: " + in + " isn't the same as Output: " + out);
+			//System.out.println("Input: " + in + " isn't the same as Output: " + out);
 			result = false;
 			return result;
 		}
